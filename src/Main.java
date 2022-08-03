e821e652ca42c9d5e0bb0cbb9bb8042f9e494ca8
Глеб <g.klochkov@worksolutions.ru>
2022-08-03 14:49:50 +0300

    Создан метод расчета ИМТ

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+
+        BmiService service = new BmiService();
+        double height = 1.6;
+        double weight = 50;
+        double bmi = service.calculate(height, weight);
+        System.out.println("Индекс массы тела = " + bmi);
+    }
+
 }
